597b20ad7a405464d935b02bd791c2a49a338c92
Saksham Sharma <146995147+capable-sharma@users.noreply.github.com>
2025-03-16 10:10:55 +0530

    Create multiplytable.java

---
+++ multiplytable.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class JavaBasics{
+public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
+   int Number;
+   System.out.println("enter the no. you want table for");
+   Number = sc.nextInt();
+
+ for (int i = 1; i<= 10; i++) {
+  System.out.println( Number + "*" + i + "=" + Number*i); 
+     
+ }
+ }
+}
